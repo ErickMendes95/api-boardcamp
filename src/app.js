e316cb159b52f5ad1af8c2cd045c05612b5691f9
Erick Mendes <erickmendesweb@gmail.com>
2023-02-10 18:49:01 -0300

    fix: package.json

--- src/app.js
+++ src/app.js
@@ -2,13 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
-import routers from "./routers/index/routers.js";
+// import routers from "./routers/index/routers.js";
 dotenv.config();
 
-const server = express();
-server.use(cors());
-server.use(express.json());
-server.use(routers);
+const app = express();
+app.use(cors());
+app.use(express.json());
+// app.use(routers);
 
 const port = process.env.PORT || 5000;
-server.listen(port, () => console.log(`Server is running in port: ${port}`));
+app.listen(port, () => console.log(`Server is running in port: ${port}`));
