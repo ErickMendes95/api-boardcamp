a06732334b1935f726ca821fe0c5d1c410109147
Erick Mendes <erickmendesweb@gmail.com>
2023-02-11 00:13:35 -0300

    feat: client controller parcialmente implementado

--- controllers/game.controllers.js
+++ controllers/game.controllers.js
@@ -5,10 +5,10 @@ export async function buscarJogos(req, res){
     try {
         const jogos = await db.query("SELECT * FROM games");
         
-        res.send(jogos.rows);
+        return res.send(jogos.rows);
 
     } catch (err) {
-        res.status(500).send(console.log(err.message));
+        return res.status(500).send(console.log(err.message));
     };
 };
 
@@ -24,16 +24,17 @@ export async function inserirJogo(req, res){
         
         const verificaNome = await db.query(`SELECT * FROM games where name= ${jogo.name}`);
 
-        if(verificaNome.rowCount === 0){
+        if(verificaNome.rowCount === 1){
             return res.sendStatus(409);
         }
 
-        await db.query(`INSERT INTO games (name, image, stockTotal, pricePerDay) VALUES (${jogo.name},${jogo.image},${jogo.stockTotal},${jogo.pricePerDay},)`);
+        await db.query(`INSERT INTO games (name, image, stockTotal, pricePerDay) 
+        VALUES (${jogo.name},${jogo.image},${jogo.stockTotal},${jogo.pricePerDay})`);
         
-        res.sendStatus(201);
+        return res.sendStatus(201);
 
     } catch (error) {
-        res.status(500).send(console.log(error.message));
+        return res.status(500).send(console.log(error.message));
     }
 
 };
