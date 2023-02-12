9ac497d09a8d5562f0fcfd2e91f3f724f2d0645a
Erick Mendes <erickmendesweb@gmail.com>
2023-02-12 19:20:49 -0300

    feat: rota post, funcao inserirAluguel implementada

--- controllers/rent.controllers.js
+++ controllers/rent.controllers.js
@@ -25,11 +25,45 @@ export async function buscarAlugueis(req, res){
 
 export async function inserirAluguel(req, res){
 
-    const {id} = req.params;
+    const aluguel = req.body; //customerId, gameId, daysRented (number, number, number)
     
     try {
+
+        if(aluguel.daysRented <= 0){
+            return res.sendStatus(400)
+        }
+
+        const customerExist = await db.query(`SELECT * FROM customers WHERE id= ${aluguel.customerId}`)
         
+        if(!customerExist){
+            return res.sendStatus(400)
+        }
+
+        const gameExist = await db.query(`SELECT * FROM games WHERE id= ${aluguel.gameId}`)
         
+        if(!gameExist){
+            return res.sendStatus(400)
+        }
+
+        const originalPrice = (gameExist.rows[0].pricePerDay)*(aluguel.daysRented)
+        const rentDate = dayjs().format("YYYY-MM-DD");
+
+        const gameDisponivel = (`SELECT * FROM rentals
+        WHERE "gameId"= ${gameExist.id} AND "returnDate"=NULL
+        `)
+
+        if(gameDisponivel.rowCount === gameExist.rows[0].stockTotal){
+            return res.sendStatus(400)
+        }
+
+        await db.query(`INSERT INTO rentals
+        ("customerId", "gameId", "daysRented", "rentDate", "returnDate", "originalPrice", "delayFee")
+        VALUES
+        (${aluguel.customerId},${aluguel.gameId}, ${aluguel.daysRented}, '${rentDate}', NULL,${originalPrice}, NULL)
+        
+        `)
+        
+        return res.sendStatus(201)
         
     } catch (error) {
         return res.status(500).send(console.log(error.message));
